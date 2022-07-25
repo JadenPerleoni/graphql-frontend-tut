7cb9bb3a9cc304ba3077c85d451b6ab322d22183
JadenPerleoni <jadenperleoni@gmail.com>
2022-07-25 08:46:18 -0700

    further in tut

---
+++ src/components/Link.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+const Link = (props) => {
+  const { link } = props;
+  return (
+    <div>
+      <div>
+        {link.description} ({link.url})
+      </div>
+    </div>
+  );
+};
+
+export default Link;
