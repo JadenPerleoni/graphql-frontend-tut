8f487793767a010f8d58c5999b7a4972df9ef0a1
JadenPerleoni <jadenperleoni@gmail.com>
2022-07-26 22:29:03 -0700

    further

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
+import { AUTH_TOKEN } from './constants';
 
 // 1
 import {
@@ -17,9 +19,19 @@ const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
