c4adce5a3e74f5add6caf596fe74dd7667526cf1
JadenPerleoni <jadenperleoni@gmail.com>
2022-07-29 09:23:45 -0700

    finished subscriptions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import "./styles/index.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { setContext } from "@apollo/client/link/context";
-import { AUTH_TOKEN } from './constants';
+import { AUTH_TOKEN } from "./constants";
+import { split } from "@apollo/client";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 // 1
 import {
@@ -29,9 +32,28 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 // 3
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
 
